6ff81f1750807eb4a4bfab8a2f700d1a39d8d918
Nikki Bright <nikkistorme@gmail.com>
2022-10-07 10:19:59 -0500

    Update Meta.js

--- components/Meta.js
+++ components/Meta.js
@@ -4,6 +4,7 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
+    <meta name="description" content="Nikki Bright is a front-end web developer working remotely out of Austin, TX." />
     <link rel="shortcut icon" href="/favicon.ico" />
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet" />
     {/* font-family: 'Josefin Sans', sans-serif; */}
